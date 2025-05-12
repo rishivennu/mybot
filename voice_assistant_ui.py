78a4c3060a87e622471d1960a4dc849b8eed8290
rishivennu <rishivennu54@gmail.com>
2025-05-12 15:47:04 +0530

    Update: describe what you changed here

--- voice_assistant_ui.py
+++ voice_assistant_ui.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
 from elevenlabs.conversational_ai.conversation import Conversation
 from elevenlabs.types import ConversationConfig
